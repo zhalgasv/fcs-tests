3ed741b5a95e24a88e96eb9634845c4b34be31e8
zhalgasadil <zhalgasvactor@gmail.com>
2025-10-30 15:10:58 +0500

    Set Playwright to run in headless mode for automated testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
       baseURL: BASE_URL,
       storageState: AUTH_FILE_PATH,
-      headless: false,
+      headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
   },
